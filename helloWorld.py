084239f9333a99eeb335c418940fe860a8d779ca
Vipin Pillai <vipin.pillai@yahoo.com>
2024-02-06 18:06:39 +0000

    added comments to line 2 and line 3

--- helloWorld.py
+++ helloWorld.py
@@ -1,4 +1,4 @@
 # Linked to L3T01 TASK
-user_input = input ("Enter your input: ")
-print (f"You have entered {user_input}")
+user_input = input ("Enter a 3 digit number: ") # requesting user inputs
+print (f"You have entered {user_input}") # print comments
 
